ce0f228db52723750424bf4d5d3a279334b49f58
MOHAMED MUSTAK M <mohamedmustak9355@gmail.com>
2026-02-20 12:02:54 +0530

    Update image_model.py

--- image_model.py
+++ image_model.py
@@ -2,18 +2,15 @@ import torch
 import torchvision.transforms as transforms
 from torchvision import models
 import numpy as np
-
 # Load pretrained ResNet50
 model = models.resnet50(pretrained=True)
 model = torch.nn.Sequential(*list(model.children())[:-1])
 model.eval()
-
 # Transform
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
-
 def extract_features(image):
     image = transform(image).unsqueeze(0)
 
